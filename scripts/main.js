bb75c21a4b6d1ec298152c252c4ec67c0f2e8a9f
VestedMango119 <r.morley624@gmail.com>
2017-04-18 21:40:35 +0100

    Added functionality for the grid to be cleared and a new number of rows to be set. The new grid will generate with the selected number of rows inthe same space as the original grid

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,61 @@
-$(document).ready(function{
-	var rows = 4;
-	var columns = 4;
-	createGrid();
+$(document).ready(function(){
+	var rows =16;
+	var square = rows;
+	var initSize = 500/(rows +2);
 
+for(var i=1; i<=rows; i++){
+	for(var j=1; j<=square; j++){
+ 		var $row = $("<div>", {"class": "rows"});
+ 		$("#grid").append($row);
+ 		$(".rows").css("width", initSize);
+ 		$(".rows").css("height", initSize);
+		}
+	}
+});
+
+var gridWidth = 500;
+var square;
+var rowSize;
+
+$("#clear").on('click', function(){
+	$(".rows").remove();
+
+
+	rows = prompt("How many rows in the next grid?");
+	square = rows;
+	rowSize = (gridWidth/rows) - 2;
+	rowSize = rowSize + "px";
+	createGrid();
 
 });
 
+/*
+$('.rows').on('click', function(){
+	$(this).css('background-color', '#ff0000');
+});
+
+$("#grid.rows").on({
+    mouseenter: function () {
+        var p = $(this).css("background-color","#ff0000");
+    },
+    mouseleave: function () {
+        $(this).attr("background-color","#ff0000");
+    }
+});
+*/
+
 function createGrid(){
+	for(var i=1; i<=rows; i++){
+	for(var j=1; j<=square; j++){
+ 		var $row = $("<div>", {"class": "rows"});
+ 		$("#grid").append($row);
+ 		$(".rows").css("width", rowSize)
+ 		$(".rows").css("height", rowSize);
 
-
+	}
 }
+}
+/*
+$('.rows').on('click', function(){
+	$(this).attr("background-color", "#ff0000");
+})*/
