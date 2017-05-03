e027bd79e25151b2aea00eaad9350ec1c72419a1
VestedMango119 <r.morley624@gmail.com>
2017-05-03 20:43:45 +0100

    Additional methods of hover functionality in JS file, and minor changes in CSS and HTML

--- scripts/main.js
+++ scripts/main.js
@@ -1,61 +1,70 @@
-$(document).ready(function(){
-	var rows =16;
-	var square = rows;
-	var initSize = 500/(rows +2);
-
-for(var i=1; i<=rows; i++){
-	for(var j=1; j<=square; j++){
- 		var $row = $("<div>", {"class": "rows"});
- 		$("#grid").append($row);
- 		$(".rows").css("width", initSize);
- 		$(".rows").css("height", initSize);
-		}
-	}
-});
+//declare variables to be used globally
 
 var gridWidth = 500;
-var square;
+var rows =16;
+var square = rows*rows;
 var rowSize;
 
+//create the initial 16x16 grid when loading the page
+$(document).ready(function(){
+	rowSize = gridWidth/(rows +2);
+	createGrid();
+});
+
+//define the clear button functionality
+//this button removes the current grid and promts the user for a new size of grid
+//the new grid is created based on the user input to fit the 500px by 500px area
 $("#clear").on('click', function(){
-	$(".rows").remove();
-
-
 	rows = prompt("How many rows in the next grid?");
-	square = rows;
+	square = rows*rows;
 	rowSize = (gridWidth/rows) - 2;
 	rowSize = rowSize + "px";
+
+	$(".rows").remove();
+
 	createGrid();
 
 });
 
-/*
-$('.rows').on('click', function(){
-	$(this).css('background-color', '#ff0000');
-});
-
-$("#grid.rows").on({
-    mouseenter: function () {
-        var p = $(this).css("background-color","#ff0000");
-    },
-    mouseleave: function () {
-        $(this).attr("background-color","#ff0000");
-    }
-});
-*/
-
+//Declare the createGrid function
+//takes the number of rows squared and creates a div in the html for each square in the grid
+//sets the size of the squares based on how many there are to fit in a 500px by 500px grid
 function createGrid(){
-	for(var i=1; i<=rows; i++){
+
 	for(var j=1; j<=square; j++){
  		var $row = $("<div>", {"class": "rows"});
  		$("#grid").append($row);
- 		$(".rows").css("width", rowSize)
+ 		$(".rows").css("width", rowSize);
  		$(".rows").css("height", rowSize);
-
 	}
 }
-}
 /*
-$('.rows').on('click', function(){
-	$(this).attr("background-color", "#ff0000");
-})*/
+$("div.rows").hover(function(){
+	$(this).css("background-color", "#ff0000");
+
+});
+*/
+/*
+    $("div#grid div.rows").mouseover(function(){
+        $(this).css("background-color", "yellow");
+    });
+    $("div#grid div.rows").mouseout(function(){
+        $(this).css("background-color", "lightgray");
+    });
+/*
+ $("div div").hover(function(){
+    $("#grid .rows").css("background-color", "yellow");
+    }, function(){
+    $(this).css("background-color", "pink");
+});
+*/
+
+$("div#grid div.rows").on({
+    mouseover: function () {
+        $(this).css("background-color","#ff0000");
+    },
+    mouseout: function () {
+        $(this).attr("background-color","#ff0000");
+    }
+});
+
