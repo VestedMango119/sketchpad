0d8bfe146dbd3aed4ba895d266f49959605e0e86
VestedMango119 <r.morley624@gmail.com>
2017-05-25 14:00:38 +0100

    Added random colour functionality which changes everytime a square is hovered over

--- scripts/main.js
+++ scripts/main.js
@@ -11,8 +11,7 @@ $(document).ready(function(){
 	createGrid();
 });
 
-//define the clear button functionality
-//this button removes the current grid and promts the user for a new size of grid
+//this button removes the current grid and prompts the user for a new size of grid
 //the new grid is created based on the user input to fit the 500px by 500px area
 $("#clear").on('click', function(){
 	rows = prompt("How many rows in the next grid?");
@@ -25,8 +24,7 @@ $("#clear").on('click', function(){
 
 });
 
-//Declare the createGrid function
-//takes the number of rows squared and creates a div in the html for each square in the grid
+//takes the number of rows and creates a div in the html for each square in the grid
 //sets the size of the squares based on how many there are to fit in a 500px by 500px grid
 function createGrid(){
 
@@ -46,3 +44,24 @@ function makeSquare(){
  		$(".rows").css("width", rowSize);
  		$(".rows").css("height", rowSize);
  	}
+
+$("#color").on("click", function(){
+	alert("Random colours on");
+	var random;
+	$(".rows").hover(function() {
+			random = randomColour();
+			$(this).css("background-color", random);
+			}, function() {
+			$(this).css("background-color", random);
+		});
+});
+
+function randomColour(){
+	random = '#' + Math.floor(Math.random() * 16777215).toString(16);
+	return random;
+}
+
+
+
+
+
